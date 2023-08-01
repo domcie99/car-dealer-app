af5c7d7d7d2425f0f0fc25568ca989410b7f5589
DomCie99 <dominikciesielski4@gmail.com>
2023-08-01 18:24:02 +0200

    Added id for sellers.

--- src/main/java/pl/ciesielski/dominik/app/cardealerapp/model/Seller.java
+++ src/main/java/pl/ciesielski/dominik/app/cardealerapp/model/Seller.java
@@ -1,17 +1,21 @@
 package pl.ciesielski.dominik.app.cardealerapp.model;
 
 public class Seller {
+    private int id;
     private String firstName;
     private String lastName;
     private String phoneNumber;
     private String email;
 
-    public Seller(String firstName, String lastName, String phoneNumber, String email) {
+    public Seller(int id, String firstName, String lastName, String phoneNumber, String email) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
+
+    public int getId() { return id; }
 
     public String getFirstName() {
         return firstName;
