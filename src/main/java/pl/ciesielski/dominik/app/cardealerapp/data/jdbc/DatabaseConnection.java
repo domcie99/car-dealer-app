181b8bf0146db066d7987b7ee4574e5559e4158d
DomCie99 <dominikciesielski4@gmail.com>
2023-08-01 18:12:52 +0200

    Create DatabaseConnection interface for database access. Create DataBaseInitializer for database initialization.

---
+++ src/main/java/pl/ciesielski/dominik/app/cardealerapp/data/jdbc/DatabaseConnection.java
@@ -0,0 +1,33 @@
+package pl.ciesielski.dominik.app.cardealerapp.data.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public interface DatabaseConnection {
+
+    default Connection getConnection() {
+        String url = "jdbc:h2:~/cardealerapp-data";
+        String username = "sa";
+        String password = "";
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    default void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ src/main/java/pl/ciesielski/dominik/app/cardealerapp/data/jdbc/DatabaseInitializer.java
@@ -0,0 +1,78 @@
+package pl.ciesielski.dominik.app.cardealerapp.data.jdbc;
+
+import pl.ciesielski.dominik.app.cardealerapp.data.jdbc.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseInitializer implements DatabaseConnection {
+
+    public void createTables() {
+        try (Connection connection = getConnection()) {
+            createClientsTable(connection);
+            createVehiclesTable(connection);
+            createTransactionsTable(connection);
+            createSellersTable(connection);
+
+            System.out.println("Tabele zostały utworzone.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createClientsTable(Connection connection) throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS clients ("
+                + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                + "first_name VARCHAR(255) NOT NULL,"
+                + "last_name VARCHAR(255) NOT NULL,"
+                + "phone_number VARCHAR(15) NOT NULL,"
+                + "email VARCHAR(255) NOT NULL,"
+                + "address VARCHAR(255) NOT NULL"
+                + ")";
+        executeUpdateQuery(connection, query);
+    }
+
+    private void createVehiclesTable(Connection connection) throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS vehicles ("
+                + "brand VARCHAR(255) NOT NULL,"
+                + "model VARCHAR(255) NOT NULL,"
+                + "yearOfProduction INT NOT NULL,"
+                + "technicalCondition VARCHAR(255) NOT NULL,"
+                + "mileage INT NOT NULL,"
+                + "vinNumber VARCHAR(255) NOT NULL PRIMARY KEY,"
+                + "registrationDate DATE NOT NULL"
+                + ")";
+        executeUpdateQuery(connection, query);
+    }
+
+    private void createTransactionsTable(Connection connection) throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS transactions ("
+                + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                + "client_id INT NOT NULL,"
+                + "vin_number VARCHAR(255) NOT NULL,"
+                + "price INT NOT NULL,"
+                + "transaction_date DATE NOT NULL,"
+                + "FOREIGN KEY (client_id) REFERENCES clients(id),"
+                + "FOREIGN KEY (vin_number) REFERENCES vehicles(vinNumber)"
+                + ")";
+        executeUpdateQuery(connection, query);
+    }
+
+    private void createSellersTable(Connection connection) throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS sellers ("
+                + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                + "first_name VARCHAR(255) NOT NULL,"
+                + "last_name VARCHAR(255) NOT NULL,"
+                + "phone_number VARCHAR(15) NOT NULL,"
+                + "email VARCHAR(255) NOT NULL"
+                + ")";
+        executeUpdateQuery(connection, query);
+    }
+
+    private void executeUpdateQuery(Connection connection, String query) throws SQLException {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.executeUpdate();
+        }
+    }
+}
