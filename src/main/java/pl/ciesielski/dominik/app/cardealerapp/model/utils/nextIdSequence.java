778482be795bb46db31f16ba56ce029daa73cef4
DomCie99 <dominikciesielski4@gmail.com>
2023-08-10 15:36:46 +0200

    Added class to get next id for given table in database using sequence.

---
+++ src/main/java/pl/ciesielski/dominik/app/cardealerapp/model/utils/nextIdSequence.java
@@ -0,0 +1,28 @@
+package pl.ciesielski.dominik.app.cardealerapp.model.utils;
+
+import pl.ciesielski.dominik.app.cardealerapp.dao.utils.DatabaseConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class nextIdSequence {
+    public static long getNextIdForTable(String tableName) {
+        String sequenceName = tableName.toLowerCase() + "_seq";
+
+        try (Connection connection = DatabaseConnectionManager.getInstance().getConnection()) {
+            String query = "SELECT NEXT VALUE FOR " + sequenceName;
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    return resultSet.getLong(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+}
